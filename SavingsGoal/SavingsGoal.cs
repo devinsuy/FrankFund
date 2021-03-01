769b1190a29a75b3f8ff6c4001a52672a1088cc7
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-02-28 18:58:50 -0800

    Created utility functions and constructors for SavingsGoal object

---
+++ SavingsGoal/SavingsGoal.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace FrankFund
+{
+    public enum contrPeriod
+    {
+        Daily = 1,
+        Weekly = 7,
+        Monthly = 30
+    }
+
+    public class SavingsGoal
+    {
+        public int SGID { get; init; }
+        public string name { get; set; }
+        public decimal goalAmt { get; set; }
+        public decimal contrAmt { get; set; }
+        public contrPeriod period { get; set; }
+        public DateTime startDate { get; init; }
+        public DateTime endDate { get; set; }
+        public int numPeriods { get; set; }
+
+        private int calcPeriodsWithDate()
+        {
+            return (int)Math.Ceiling((endDate - startDate).TotalDays / (int)period);
+        }
+
+        private int calcPeriodsWithAmt()
+        {
+            return (int) Math.Ceiling(goalAmt / contrAmt);
+        }
+
+        private decimal calcContrAmt()
+        {
+            return Math.Round(goalAmt / numPeriods, 2);
+        }
+
+        private DateTime calcEndDate()
+        {
+            int numDays = numPeriods * (int)period;
+            DateTime endDate = startDate.AddDays(numDays);
+            return endDate;
+        }
+
+
+        // Constructor: create SavingsGoal with amount by endDate
+        public SavingsGoal(string name, decimal goalAmt, contrPeriod period, DateTime endDate)
+        {
+            this.name = name;
+            this.goalAmt = goalAmt;
+            this.period = period;
+            this.startDate = DateTime.Now;
+
+            this.endDate = endDate;
+            this.numPeriods = calcPeriodsWithDate();
+            this.contrAmt = calcContrAmt();
+        }
+
+        // Constructor: create SavingsGoal with amount and specified contribution
+        public SavingsGoal(string name, decimal goalAmt, contrPeriod period, decimal contrAmt)
+        {
+            this.name = name;
+            this.goalAmt = goalAmt;
+            this.period = period;
+            this.startDate = DateTime.Now;
+
+            this.numPeriods = calcPeriodsWithAmt();
+            this.endDate = calcEndDate();
+            this.contrAmt = contrAmt;
+        }
+    }
+}
