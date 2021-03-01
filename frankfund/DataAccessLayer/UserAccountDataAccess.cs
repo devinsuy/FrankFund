fd27ac8957c5d26c5a5fe05096f297cc388e9328
Autumn Nguyen <38737498+seasonfall27@users.noreply.github.com>
2021-02-28 21:03:41 -0800

    FRAN-46 Set up initial project architecture

---
+++ frankfund/DataAccessLayer/UserAccountDataAccess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.Cloud.BigQuery.V2;
+
+namespace DataAccessLayer
+{
+    public class UserAccountDataAccess
+    {
+        private readonly BigQueryClient client;
+        public UserAccountDataAccess(BigQueryClient client)
+        {
+            this.client = client;
+        }
+
+        public BigQueryResults GetUserAccountUsingID(string ID)
+        {
+            BigQueryResults retrievedUserAccount = null;
+            string query = @"SELECT
+                CONCAT(
+                    'https://stackoverflow.com/questions/',
+                    CAST(id as STRING)) as url, view_count
+                FROM `bigquery-public-data.stackoverflow.posts_questions`
+                WHERE tags like '%google-bigquery%'
+                ORDER BY view_count DESC
+                LIMIT 10";
+            retrievedUserAccount = client.ExecuteQuery(query, parameters: null);
+            return retrievedUserAccount;
+        }
+    }
+}
