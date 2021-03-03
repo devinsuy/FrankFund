eb0a47a8be683c581a8b0fb18995441d47314cdb
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-03-02 23:13:52 -0800

    Demo of SavingsGoal layered architecture, instantiate SavingsGoal, serialize, parse to JSON, write to BigQuery

--- frankfund/ServiceLayer/debug.cs
+++ frankfund/ServiceLayer/debug.cs
@@ -10,11 +10,80 @@ namespace ServiceLayer
     {
         static void Main(string[] args)
         {
+            // ---------------------------------------------- GCP AUTH ----------------------------------------------
+
             // Replace with path to wherever Auth<name>.json file is on your local machine
-            string pathToCreds = "Credentials/AuthDevin.json";  
+            string pathToCreds = "/Users/devin/Documents/GitHub/FrankFund/Credentials/AuthDevin.json";  
             System.Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", pathToCreds);
 
-            Console.WriteLine("Hello world");
+
+            // ---------------------------------------------- Sample Test of Savings Goal Layers ----------------------------------------------
+
+            // SavingsGoalService layer functionality 
+            SavingsGoalService sgService = new SavingsGoalService();
+            int SGID = sgService.getNextAvailSGID();
+            DateTime endDate = new DateTime(2021, 12, 25, 0, 0, 0).Date;
+            SavingsGoal sampleGoal = new SavingsGoal(SGID, "Christmas Gift", (decimal)250.25, contrPeriod.Monthly, endDate);
+
+            // Print goal summary
+            Console.WriteLine("Savings Goal Summary:\n---------------------\n");
+            Console.WriteLine(sampleGoal);
+
+            // Serialize the runtime object to String[] and JSON string
+            string[] serialized = sgService.serializeSavingsGoal(sampleGoal);
+            Console.WriteLine("\nSavingsGoal Serialized:\n-----------------------\n");
+            foreach(string attr in serialized){
+                Console.WriteLine("   " + attr);
+            }
+            sgService.getJSON(sampleGoal);
+
+
+            // Write SavingsGoal object to DB
+            sgService.writeSavingsGoal(sampleGoal);
+
+
+            /*
+                Console Output:
+                    Running Query:
+                    --------------
+                    SELECT MAX(SGID) AS maxSGID FROM frankfund.FrankFund.SavingsGoals
+
+                    Savings Goal Summary:
+                    ---------------------
+
+                    "Christmas Gift" Savings Goal
+                    For the amount of $250.25
+                    Began on 2021-03-02 and ends on 2021-12-25
+                    Requires a Monthly contribution of $25.02 for 10 months
+
+                    SavingsGoal Serialized:
+                    -----------------------
+
+                    1
+                    Christmas Gift
+                    250.25
+                    25.02
+                    Monthly
+                    2021-03-02
+                    2021-12-25
+
+                    SavingsGoal JSON Representation:
+                    --------------------------------
+                    {"SGID":1,"Name":"Christmas Gift","GoalAmt":250.25,"ContrAmt":25.02,"Period":"Monthly","StartDate":"2021-03-02","EndDate":"2021-12-25"}
+
+                    {"SGID":1,"Name":"Christmas Gift","GoalAmt":250.25,"ContrAmt":25.02,"Period":"Monthly","StartDate":"2021-03-02","EndDate":"2021-12-25"}
+
+                    Running Insert Query:
+                    ---------------------
+                    INSERT INTO frankfund.FrankFund.SavingsGoals VALUES (1,"Christmas Gift",250.25,25.02,"Monthly","2021-03-02","2021-12-25")
+            */
+
+
+            // ---------------------------------------------- End Savings Goal Sample Test  ----------------------------------------------
+
+
+
+
         }
     }
 }
