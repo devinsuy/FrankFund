37687e93c20f3eb14040f408101ba363cc7590da
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-03-02 19:18:04 -0800

    Abstracted BigQuery client to DataHelper wrapper class

---
+++ frankfund/DataAccessLayer/DataHelper.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using Google.Cloud.BigQuery.V2;
+
+namespace DataAccessLayer
+{
+    public class DataHelper
+    {
+        private readonly BigQueryClient client;
+        private readonly string projectID;
+        private readonly string datasetID;
+        public DataHelper()
+        {
+            this.projectID = "frankfund";                                                                        // GCP project id is always lowercase
+            this.datasetID = "FrankFund";
+            this.client = BigQueryClient.Create(projectID);                    
+        }
+
+        public string getFullQualifiedTableName(string tableID){
+            return this.projectID + "." + this.datasetID + "." + tableID;
+        }
+
+        public BigQueryResults executeQuery(string query, IEnumerable<BigQueryParameter> parameters=null)
+        {
+            return this.client.ExecuteQuery(query, parameters);
+        }
+
+
+
+    }
+}
---
+++ frankfund/DataAccessLayer/SavingsGoalDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Google.Cloud.BigQuery.V2;
+
+namespace DataAccessLayer
+{
+    public class SavingsGoalDataAccess
+    {
+        private readonly DataHelper dataHelper;
+        public SavingsGoalDataAccess()
+        {
+            this.dataHelper = new DataHelper();
+        }
+
+        public BigQueryResults GetSavingsGoalUsingID(string ID)
+        {
+            DataHelper 
+            BigQueryResults retrievedSavingsGoal = null;
+
+            var client = BigQueryClient.Create(projectID);                    
+            var table = client.GetTable(datasetID, tableName);               
+            var query = $"SELECT * FROM {projectID}.{datasetID}.{tableName} ORDER BY AccountID DESC";           // Table selection is always in the form: projectID.datasetID.tableName
+            var queryResults = client.ExecuteQuery(query, parameters: null);
+            Console.WriteLine("Executing query: " + query);
+            return retrievedUserAccount;
+        }
+        
+    }
+}
