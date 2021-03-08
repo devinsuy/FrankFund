65a73048b3a2ce47dca632f7349ebcb0f38abbea
kennethtran590 <48599140+kennethtran590@users.noreply.github.com>
2021-03-08 12:43:53 -0800

    Create TransactionService.cs

---
+++ frankfund/ServiceLayer/TransactionService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccessLayer;
+using Google.Cloud.BigQuery.V2;
+using DataAccessLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceLayer
+{
+    class TransactionService
+    {
+        private readonly TransactionDataAccess TransactionDataAccess;
+
+        public TransactionService()
+        {
+            this.TransactionDataAccess = new TransactionDataAccess();
+        }
+
+        public void AddTransaction(Transaction transaction)
+        {
+             
+        }
+    }
+}
