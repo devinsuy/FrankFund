d3056ae2271825d0d27f122133fe3bbcac0b871d
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-03-12 17:42:55 -0800

    Added insert sample data DML

---
+++ BigQuery/DML/Insert_Receipts.sql
@@ -0,0 +1,11 @@
+INSERT INTO frankfund.FrankFund.Receipts
+VALUES
+(1,8,'null','2021-03-10',"Movie"),
+(2,3,'null','2021-02-01',"McDonalds"),
+(3,13,'null','2021-04-15',"Doctors Visit"),
+(4,12,'null','2021-03-15',"Doctors Visit"),
+(5,18,'null','2021-02-01',"Walmart"),
+(6,20,'null','2021-03-10',"Cap and gown"),
+(7,21,'null','2021-04-28',"Birthday cake"),
+(8,22,'null','2021-03-21',"Bookstore"),
+(9,23,'null','2021-04-23',"Boba")
---
+++ BigQuery/DML/Insert_Subscriptions.sql
@@ -0,0 +1,7 @@
+INSERT INTO frankfund.FrankFund.Subscriptions
+VALUES
+(1,2,null,'2021-01-14',"Netflix",8.99,"Monthly"),
+(2,1,null,'2021-02-10',"Monthly tea box",25.00,"Monthly"),
+(3,3,null,'2021-03-20',"CauseBox",54.95,"Monthly"),
+(4,3,null,'2021-04-21',"Tonishi Me Box",20.75,"Monthly"),
+(5,4,null,'2021-03-02',"Disney Plus",6.99,"Monthly")
