3d4f415655203f3ff0ead1f7aec6c0c50a3f7e4b
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-03-12 17:43:13 -0800

    Added AccountID column to associate subscription with a user account

--- BigQuery/DDL/Subscriptions_DDL.sql
+++ BigQuery/DDL/Subscriptions_DDL.sql
@@ -1,5 +1,6 @@
 CREATE TABLE FrankFund.Subscriptions(
     SID 			INT64 		NOT NULL,
+    AccountID       INT64       NOT NULL,          
     RID 			INT64,
     PurchaseDate 	DATE 		NOT NULL,
     Notes 			STRING,
