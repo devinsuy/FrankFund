6c3c03d0e8b9dae429cd3fe6cc29f291d848f8e1
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-03-15 11:46:13 -0700

    Finalize landing deployment

--- frankfund/REST/Startup.cs
+++ frankfund/REST/Startup.cs
@@ -38,19 +38,18 @@ namespace REST
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            // Serve landing page for non API routing
             DefaultFilesOptions options = new DefaultFilesOptions();
             options.DefaultFileNames.Clear();
             options.DefaultFileNames.Add("Index.html");
