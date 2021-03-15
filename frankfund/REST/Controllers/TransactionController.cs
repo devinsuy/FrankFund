cacafd9a2960a78c47482bf7e9875007873b2fc1
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-03-14 20:20:32 -0700

    Deployed REST api endpoint to retrieve JSON of traction by TID

---
+++ frankfund/REST/Controllers/TransactionController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ServiceLayer;
+
+
+namespace REST.Controllers
+{
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly ILogger<TransactionController> _logger;
+        private readonly APIHelper api;
+        private readonly TransactionService ts;
+
+        public TransactionController(ILogger<TransactionController> logger)
+        {
+            _logger = logger;
+            api = new APIHelper();
+            ts = new TransactionService();
+        }
+
+        [Route("api/TID={TID}&apikey={apiKey}")]
+        [HttpGet]
+        public IActionResult GetSGByID(int TID, string apiKey)
+        {
+            if (!api.validAPIKey(apiKey))
+            {
+                return new UnauthorizedResult();
+            }
+            if (TID < 1)
+            {
+                return BadRequest();
+            }
+            long maxTID = ts.getNextAvailTID() - 1;
+            if (TID > maxTID)
+            {
+                return NoContent();
+            }
+            return new OkObjectResult(ts.getJSON(ts.GetTransactionUsingID(TID)));
+        }
+    }
+}
