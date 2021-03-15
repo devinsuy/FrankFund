92cb64f88a47016001f26b9259535b13ab3a76f5
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-03-14 19:38:46 -0700

    Implemented UserAccount String serialization and parse JSON

--- frankfund/ServiceLayer/UserAccountService.cs
+++ frankfund/ServiceLayer/UserAccountService.cs
@@ -70,6 +70,42 @@ namespace ServiceLayer
             return user;
         }
 
+        /*
+        Serialize a UserAccount object into a String array
+            Returns: A string array with each element in order of its column attribute (see UserAccount DB schema)
+        */
+        public string[] serializeUserAccount(UserAccount acc)
+        {
+            return new string[] {
+                acc.AccountID.ToString(),
+                acc.AccountUsername,
+                acc.EmailAddress,
+                acc.PasswordHash
+            };
+        }
+
+        /*
+        Convert a UserAccount object into JSON format
+            Params: A UserAccount object to convert
+            Returns: The JSON string representation of the object
+        */
+        public string getJSON(UserAccount s)
+        {
+            if (s == null)
+            {
+                return "{}";
+            }
+            string[] serialized = serializeUserAccount(s);
+            string jsonStr = "{"
+                + $"\"AccountID\":{serialized[0]},"
+                + $"\"AccountUsername\":\"" + serialized[1] + "\","
+                + $"\"EmailAddress\":\"" + serialized[2] + "\","
+                + $"\"Password\":\"" + serialized[3] + "\""
+            + "}";
+
+            return jsonStr;
+        }
+
         public long getNextAvailID(){
             return UserAccountDataAccess.getNextAvailID();
         }
