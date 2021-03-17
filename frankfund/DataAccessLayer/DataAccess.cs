5c131aed9a4e9e63a4aac93d15e7e0b94c8386ef
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-03-16 18:16:15 -0700

    Defined layer interfaces

---
+++ frankfund/DataAccessLayer/DataAccess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Google.Cloud.BigQuery.V2;
+
+namespace DataAccessLayer
+
+{
+    public interface DataAccess<T>
+    {
+
+        // ---------------------------------------- Utility Methods ----------------------------------------
+
+        /* Wrapper method, implement using the following line:
+           return this.dataHelper.getNextAvailID(this.tableID) */
+        public long getNextAvailID();
+
+
+
+        // ---------------------------------------- Read, Write, Edit, Delete -------------------------------
+
+
+        /* Use DataHelper.query() to query BigQuery to READ the records for the
+           object with the given PK Identifier. Return results to ServiceLayer */
+        public BigQueryResults getUsingID(long ID);
+
+
+        // Use DataHelper.query() to WRITE a newly created string serialized object into BigQuery
+        public void write(string[] serializedObj);
+
+        // Use DataHelper.query() to DELETE an object from BigQuery given its PK identifier
+        public void delete(long ID);
+
+        /* Use DataHelper.query() to REWRITE an existing object that changed at runtime
+           This method should call delete(long ID) followed by write(string[] serializedObj) */
+        public void rewrite(string[] serializedObj);
+
+
+
+    }
+}
---
+++ frankfund/ServiceLayer/Service.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace ServiceLayer
+{
+    public interface Service<T>
+    {
+        // ---------------------------------------- Utility Methods ----------------------------------------
+
+        // Serialize object into string array of the objects attributes
+        public string[] serialize(T obj);
+
+        // Serialize object into a valid json string (Check if json is valid here: https://jsonlint.com/)
+        public string getJSON(T obj);
+
+        // Get the next available Primary Key ID for the object from BigQuery via DataAccess Layer
+        public long getNextAvailID();
+
+
+
+        // ---------------------------------------- Read, Write, Edit, Delete -------------------------------
+
+        /* Retrieve from BigQuery via DataAccess Layer and return object 
+           (EX: return a reinstantiated Transaction from DB records) */
+        public T getUsingID(long ID);
+
+
+        /* Use DataAccess Layer to write or rewrite a object into BigQuery
+               (method should have a way of checking whether the class object changed during runtime
+               to avoid redundant writing. Use a changed boolean to implement this)
+           Should not call DataAccess write() if did not change */
+        public void write(T obj);
+
+
+        // Use DataAccess Layer to delete a object from BigQuery given its primary key identifier
+        public void delete(long ID);
+
+    }
+}
