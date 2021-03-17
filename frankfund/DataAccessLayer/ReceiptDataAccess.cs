7b1b1f13e6a2adf197eeb8b26ea8cf2853bde444
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-03-16 20:41:25 -0700

    Added Receipt layers with interface TODO stubs

---
+++ frankfund/DataAccessLayer/ReceiptDataAccess.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Google.Cloud.BigQuery.V2;
+namespace DataAccessLayer
+{
+    public class ReceiptDataAccess: DataAccess
+    {
+        private readonly DataHelper dataHelper;
+        private readonly string tableID;
+        public ReceiptDataAccess()
+        {
+            this.dataHelper = new DataHelper();
+            this.tableID = this.dataHelper.getQualifiedTableName("Receipts");
+        }
+
+
+        public BigQueryResults getUsingID(long ID)
+        {
+            string query = "";
+            return this.dataHelper.query(query, parameters: null);
+        }
+
+        // TODO: Use DataHelper.query() to WRITE a newly created string serialized object into BigQuery
+        public void write(string[] serializedAcc)
+        {
+
+        }
+
+
+        // TODO: Use DataHelper.query() to DELETE an object from BigQuery given its PK identifier
+        public void delete(long accID)
+        {
+
+        }
+
+        /* TODO: Use DataHelper.query() to REWRITE an existing object that changed at runtime
+           This method should call delete(long ID) followed by write(string[] serializedObj) */
+        public void update(string[] serializedAcc)
+        {
+
+        }
+
+        public long getNextAvailID()
+        {
+            return this.dataHelper.getNextAvailID(this.tableID);
+        }
+    }
+}
---
+++ frankfund/ServiceLayer/ReceiptService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using DataAccessLayer.Models;
+using DataAccessLayer;
+namespace ServiceLayer
+{
+    public class ReceiptService : Service<Receipt>
+    {
+        private readonly ReceiptDataAccess dataAccess;
+        public ReceiptService()
+        {
+            this.dataAccess = new ReceiptDataAccess();
+        }
+
+        public void delete(long accID)
+        {
+
+        }
+
+        // TODO: Use DataAccess Layer to write a NEWLY CREATED object into BigQuery
+        public void write(Receipt acc)
+        {
+
+        }
+
+        // TODO: Query DB to reinstantate and return a receipt object with the given receipt id
+        public Receipt getUsingID(long ID)
+        {
+            return null;
+        }
+
+        /* TODO:
+           Write a modified object's changed to BigQuery via DataAccess Layer 
+               (method should have a way of checking whether the class object changed during runtime
+               to avoid redundant writing. Use a changed boolean to implement this)
+           Should not call DataAccess update() if did not change */
+        public void update(Receipt r)
+        {
+
+        }
+
+        /*
+        TODO: Convert a Receipt object into JSON format
+            Params: A UserAccount object to convert
+            Returns: The JSON string representation of the object
+        */
+        public string getJSON(Receipt r)
+        {
+            return null;
+        }
+
+        /*
+        TODO: Serialize a Receipt object into a String array
+            Returns: A string array with each element in order of its column attribute (see Receipts DB schema)
+        */
+        public string[] serialize(Receipt r)
+        {
+            return null;
+        }
+
+        public long getNextAvailID()
+        {
+            return dataAccess.getNextAvailID();
+        }
+    }
+
+}
