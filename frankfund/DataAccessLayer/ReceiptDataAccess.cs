f5a3f525e9e4534398d4e1e3d402b341962d60e6
Rachel Pai <44449816+pairachel@users.noreply.github.com>
2021-03-17 00:05:53 -0700

    Tried to implemdent write function
    Doesn't quite work yet.

--- frankfund/DataAccessLayer/ReceiptDataAccess.cs
+++ frankfund/DataAccessLayer/ReceiptDataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Google.Cloud.BigQuery.V2;
+
 namespace DataAccessLayer
 {
     public class ReceiptDataAccess: DataAccess
@@ -12,19 +13,48 @@ namespace DataAccessLayer
             this.tableID = this.dataHelper.getQualifiedTableName("Receipts");
         }
 
-
         public BigQueryResults getUsingID(long ID)
         {
-            string query = "";
+            string query = $"SELECT * from {this.tableID} WHERE RID = {ID}";
             return this.dataHelper.query(query, parameters: null);
         }
 
         // TODO: Use DataHelper.query() to WRITE a newly created string serialized object into BigQuery
-        public void write(string[] serializedAcc)
+        public void write(string[] serializedObj)
         {
+            _ = $"INSERT INTO {this.tableID} VALUES ("
+                + serializedObj[0] + "," //receipt ID (RID)
+                + serializedObj[1] + "," //transaction ID (TID)
+                + $"\"{serializedObj[2]}\"," //imageURL
+                + $"\"{serializedObj[3]}\"," //purchaseDate 
+                + $"\"{serializedObj[4]}\")"; //Notes 
 
         }
+        /* public void write(string[] serializedObj, bool newlyCreated, bool changed)
+        {
+            string query;
 
+            if (!newlyCreated)
+            {
+                if (changed)
+                {
+                    query = $"DELETE FROM {this.tableID} WHERE RID={serializedObj[0]}";
+                    Console.WriteLine("Receipt with RID " + serializedObj[0] + "was changed, updating records");
+                    this.dataHelper.query(query);
+                }
+                else
+                    return;
+            }
+
+            query = $"INSERT INTO {this.tableID} VALUES ("
+                + serializedObj[0] + "," //receipt ID (RID)
+                + serializedObj[1] + "," //transaction ID (TID)
+                + $"\"{serializedObj[2]}\"," //imageURL
+                + $"\"{serializedObj[3]}\"," //purchaseDate 
+                + $"\"receipt[4]\")"; //Notes 
+
+        }
+        */
 
         // TODO: Use DataHelper.query() to DELETE an object from BigQuery given its PK identifier
         public void delete(long accID)
@@ -43,5 +73,16 @@ namespace DataAccessLayer
         {
             return this.dataHelper.getNextAvailID(this.tableID);
         }
+
+        // Overload wrappers to cast BigQuery Numeric type to C# decimal type
+        public decimal castBQNumeric(BigQueryNumeric val)
+        {
+            return this.dataHelper.castBQNumeric(val);
+        }
+        public decimal castBQNumeric(object val)
+        {
+            return this.dataHelper.castBQNumeric(val);
+        }
+
     }
 }
