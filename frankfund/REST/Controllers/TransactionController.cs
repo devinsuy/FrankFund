7cbd2bfeb286bcf2ba259e1625224a331eaf5b91
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-03-18 18:37:12 -0700

    Finished Transaction GET/PUT/POST/DELETE/PATCH

--- frankfund/REST/Controllers/TransactionController.cs
+++ frankfund/REST/Controllers/TransactionController.cs
@@ -14,20 +14,33 @@ namespace REST.Controllers
         private readonly ILogger<TransactionController> _logger;
         private readonly APIHelper api;
         private readonly TransactionService ts;
-        private readonly HashSet<string> attributes;
+        private readonly HashSet<string> createAttr;
+        private readonly HashSet<string> updateAttr;
 
         public TransactionController(ILogger<TransactionController> logger)
         {
             _logger = logger;
             api = new APIHelper();
             ts = new TransactionService();
-            attributes = new HashSet<string>
+
+            // Attributes that should be specified in request payload if creating a NEW transaction
+            // Newly created transaction with assign dateTransactionEntered to DateTime.now(), it should not be passed
+            createAttr = new HashSet<string>
+            {
+                "SGID", "AccountID", "TransactionName", "Amount", "DateTransactionMade",
+                "IsExpense", "TransactionCategory"
+            };
+
+            // Attributes that should be specified in request payload if updating an EXISTING transaction
+            updateAttr = new HashSet<string>
             {
                 "SGID", "AccountID", "TransactionName", "Amount", "DateTransactionMade",
                 "DateTransactionEntered", "IsExpense", "TransactionCategory"
             };
         }
 
+        // Retrieve a transaction with the given TID,
+        // returns Http 204 NoContent if doesn't exist
         [Route("api/TID={TID}&apikey={apiKey}")]
         [HttpGet]
         public IActionResult GetByID(int TID, string apiKey)
@@ -43,6 +56,8 @@ namespace REST.Controllers
             return api.serveJson(ts.getJSON(ts.getUsingID(TID)));
         }
 
+
+        // Delete a transaction, no effect if a transaction with the given TID doesn't exist
         [Route("api/TID={TID}&apikey={apiKey}")]
         [HttpDelete]
         public IActionResult DeleteByID(int TID, string apiKey)
@@ -60,8 +75,141 @@ namespace REST.Controllers
         }
 
 
+        // Create a new transaction with the given TID.
+        // Returns Http 409 Conflict if already exists
+        [Route("api/TID={TID}&apikey={apiKey}")]
+        [HttpPost]
+        public IActionResult CreateByID(int TID, string apiKey)
+        {
+            if (!api.validAPIKey(apiKey))
+            {
+                return new UnauthorizedResult();
+            }
+            if (TID < 1)
+            {
+                return BadRequest();
+            }
+
+            // Validate that the POST request contains all necessary attributes to create a NEW transaction and nothing more
+            HashSet<string> reqAttributes = new HashSet<string>(Request.Form.Keys);
+            if (!reqAttributes.SetEquals(createAttr))
+            {
+                return BadRequest();
+            }
+
+            // POST should be used only to create a new Transaction, not allowed if Transaction with given TID already exists
+            Transaction t = ts.getUsingID(TID);
+            if(t != null)
+            {
+                return Conflict();
+            }
+
+            // Create the transaction with the given TID using the POST payload
+            var req = Request.Form;
+            t = new Transaction(
+                    TID: TID,
+                    accountID: long.Parse(req["AccountID"]),
+                    SGID: long.Parse(req["SGID"]),
+                    transactionName: req["TransactionName"],
+                    amount: decimal.Parse(req["Amount"]),
+                    dateTransactionMade: DateTime.Parse(req["DateTransactionMade"]),
+                    isExpense: System.Convert.ToBoolean(req["IsExpense"]),
+                    transactionCategory: req["TransactionCategory"]
+                );
+
+            // Write the new transaction
+            ts.write(t);
+            return new OkResult();
+        }
+
+
+        // Update an existing transaction or create if not exists
         [Route("api/TID={TID}&apikey={apiKey}")]
         [HttpPut]
+        public IActionResult UpdateAllByID(int TID, string apiKey)
+        {
+            if (!api.validAPIKey(apiKey))
+            {
+                return new UnauthorizedResult();
+            }
+            if (TID < 1)
+            {
+                return BadRequest();
+            }
+
+            var req = Request.Form;
+            HashSet<string> reqAttributes = new HashSet<string>(Request.Form.Keys);
+            Transaction t = ts.getUsingID(TID);
+
+            // Create the transaction with the given TID if it doesn't exist
+            if(t == null)
+            {
+                // PUT requires request to provide key,value pairs for EVERY Transaction attribute except dateTransactionEntered
+                if (!reqAttributes.SetEquals(createAttr))
+                {
+                    return BadRequest();
+                }
+                try
+                {
+                    t = new Transaction(
+                            TID: TID,
+                            accountID: long.Parse(req["AccountID"]),
+                            SGID: long.Parse(req["SGID"]),
+                            transactionName: req["TransactionName"],
+                            amount: decimal.Parse(req["Amount"]),
+                            dateTransactionMade: DateTime.Parse(req["DateTransactionMade"]),
+                            isExpense: System.Convert.ToBoolean(req["IsExpense"]),
+                            transactionCategory: req["TransactionCategory"]
+                        );
+                }
+                // Formatting or improper data typing raised exception, bad request
+                catch
+                {
+                    return BadRequest();
+                }
+
+                // Write the new transaction
+                ts.write(t);
+            }
+
+            // Otheriwse fufill the PUT request and update the corresponding transaction 
+            else
+            {
+                // HTTP PUT request to update an EXISTING transaction requires ALL fields of the transaction to be specified
+                if(!reqAttributes.SetEquals(updateAttr))
+                {
+                    return BadRequest();
+                }
+
+                // TID and AccountID are never modifiable
+                try
+                {
+                    t.setSGID(long.Parse(req["SGID"]));
+                    t.setTransactionName(req["TransactionName"]);
+                    t.setAmount(decimal.Parse(req["Amount"]));
+                    t.setDateTransactionMade(DateTime.Parse(req["DateTransactionMade"]));
+                    t.setDateTransactionEntered(DateTime.Parse(req["DateTransactionEntered"]));
+                    t.setIsExpense(bool.Parse(req["IsExpense"]));
+                    t.setTransactionCategory(req["TransactionCategory"]);
+                }
+                // Formatting or improper data typing raised exception, bad request
+                catch
+                {
+                    return BadRequest();
+                }
+
+                // Write changes, if any
+                ts.update(t);
+            }
+
+            return new OkResult();
+        }
+
+
+        // Modify an existing transaction without specifying all attributes in payload,
+        // returns Http 404 Not found if doesn't exist
+        [Route("api/TID={TID}&apikey={apiKey}")]
+        [HttpPatch]
         public IActionResult UpdateByID(int TID, string apiKey)
         {
             if (!api.validAPIKey(apiKey))
@@ -73,62 +221,64 @@ namespace REST.Controllers
                 return BadRequest();
             }
 
-            // Validate if the attributes of the PUT request are attributes of a Transaction
-            // PUT requires request to provide key,value pairs for EVERY Transaction attribute
+            // Validate the attributes of the PATCH request, each attribute specified
+            // in the request must be an attribute of a Transaction
             HashSet<string> reqAttributes = new HashSet<string>(Request.Form.Keys);
-            if (!api.validAttributes(attributes, reqAttributes)){
+            if (!api.validAttributes(updateAttr, reqAttributes))
+            {
                 return BadRequest();
             }
 
             var req = Request.Form;
             Transaction t = ts.getUsingID(TID);
 
-            // Create the transaction with the given TID if it doesn't exist
-            if(t == null)
+            // Http POST cannot update a transaction that does not exist
+            if (t == null)
             {
-                // Newly created transaction with assign dateTransactionEntered to DateTime.now(), it should not be passed
-                if (reqAttributes.Count != attributes.Count - 1)
-                {
-                    return BadRequest();
-                }
-
-                t = new Transaction(                                        
-                        TID:                    TID,
-                        accountID:              long.Parse(req["AccountID"]),
-                        SGID:                   long.Parse(req["SGID"]),
-                        transactionName:        req["TransactionName"],
-                        amount:                 decimal.Parse(req["Amount"]),
-                        dateTransactionMade:    DateTime.Parse(req["DateTransactionMade"]),
-                        isExpense:              System.Convert.ToBoolean(req["IsExpense"]),
-                        transactionCategory:    req["TransactionCategory"]
-                    );
-
-                // Write the new transaction
-                ts.write(t);
+                return NotFound();
             }
 
-            // Otheriwse fufill the PUT request and update the corresponding transaction 
-            else
+            // Otherwise fufill the POST request and update the corresponding transaction
+            // Http POST may only specify a few attributes to update or provide all of them
+            // AccountID is not an updatable attribute
+
+            // Update the transaction with the specified POST attributes
+            try
             {
-                // HTTP PUT request to update an EXISTING transaction requires all fields of the transaction to be specified
-                if(reqAttributes.Count != attributes.Count)
+                if (reqAttributes.Contains("SGID"))
                 {
-                    return BadRequest();
+                    t.setSGID(long.Parse(req["SGID"]));
                 }
-
-                // TID and AccountID are never modifiable
-                t.setSGID(long.Parse(req["SGID"]));
-                t.setTransactionName(req["TransactionName"]);
-                t.setAmount(decimal.Parse(req["Amount"]));
-                t.setDateTransactionMade(DateTime.Parse(req["DateTransactionMade"]));
-                t.setDateTransactionEntered(DateTime.Parse(req["DateTransactionMade"]));
-                t.setIsExpense(bool.Parse(req["IsExpense"]));
-                t.setTransactionCategory(req["TransactionCategory"]);
-
-                // Write changes, if any
-                ts.update(t);
+                if (reqAttributes.Contains("TransactionName"))
+                {
+                    t.setTransactionName(req["TransactionName"]);
+                }
+                if (reqAttributes.Contains("Amount"))
+                {
+                    t.setAmount(decimal.Parse(req["Amount"]));
+                }
+                if (reqAttributes.Contains("DateTransactionMade")) {
+                    t.setDateTransactionMade(DateTime.Parse(req["DateTransactionMade"]));
+                }
+                if (reqAttributes.Contains("DateTransactionEntered")){
+                    t.setDateTransactionEntered(DateTime.Parse(req["DateTransactionEntered"]));
+                }
+                if (reqAttributes.Contains("IsExpense"))
+                {
+                    t.setIsExpense(bool.Parse(req["IsExpense"]));
+                }
+                if (reqAttributes.Contains("TransactionCategory"))
+                {
+                    t.setTransactionCategory(req["TransactionCategory"]);
+                }
+            }
+            // Formatting or improper data typing raised exception, bad request
+            catch {
+                return BadRequest();
             }
 
+            // Write changes, if any
+            ts.update(t);
             return new OkResult();
         }
 
