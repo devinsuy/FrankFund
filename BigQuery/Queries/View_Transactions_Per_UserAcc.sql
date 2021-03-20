a937be4ea1926a75ce2bc0a6db9e7feb325f3e17
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-03-20 15:00:11 -0700

    Update View_Transactions_Per_UserAcc.sql

--- BigQuery/Queries/View_Transactions_Per_UserAcc.sql
+++ BigQuery/Queries/View_Transactions_Per_UserAcc.sql
@@ -1,4 +1,5 @@
-SELECT * FROM FrankFund.Transactions f
+SELECT a.accountID, t.TID, t.TransactionName, t.amount, t.DateTransactionMade, t.TransactionCategory
+FROM FrankFund.Transactions t
 INNER JOIN FrankFund.Accounts a
-ON f.AccountID = a.AccountID
-ORDER BY a.AccountID ASC
+ON t.AccountID = a.AccountID
+ORDER BY a.AccountID, TransactionCategory
