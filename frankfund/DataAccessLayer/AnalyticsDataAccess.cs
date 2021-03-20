1ea9a50e45305c094541559765d972903d668153
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-03-20 16:12:19 -0700

    Added spending per category for a given UserAccount

---
+++ frankfund/DataAccessLayer/AnalyticsDataAccess.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Google.Cloud.BigQuery.V2;
+
+namespace DataAccessLayer
+{
+    public class AnalyticsDataAccess
+    {
+        private readonly DataHelper dataHelper;
+        private readonly string[] tables;
+
+        public AnalyticsDataAccess()
+        {
+            this.dataHelper = new DataHelper();
+            this.tables = new string[]
+            {
+                this.dataHelper.getQualifiedTableName("Accounts"),
+                this.dataHelper.getQualifiedTableName("Receipts"),
+                this.dataHelper.getQualifiedTableName("SavingsGoals"),
+                this.dataHelper.getQualifiedTableName("Subscriptions"),
+                this.dataHelper.getQualifiedTableName("Transactions")
+            };
+        }
+
+        public BigQueryResults getSpendingPerCategory(long accID)
+        {
+            string query = "SELECT a.accountID, t.TransactionCategory, SUM(t.amount) AS CategoryTotal"
+                + " FROM FrankFund.Transactions t"
+                + " INNER JOIN FrankFund.Accounts a"
+                + " ON t.AccountID = a.AccountID"
+                + $" WHERE a.accountID = {accID}"
+                + " GROUP BY a.AccountID, TransactionCategory"
+                + " ORDER BY SUM(t.amount) DESC";
+
+            return this.dataHelper.query(query, parameters: null);
+        }
+
+        // Overload wrappers to cast BigQuery Numeric type to C# decimal type
+        public decimal castBQNumeric(BigQueryNumeric val)
+        {
+            return this.dataHelper.castBQNumeric(val);
+        }
+        public decimal castBQNumeric(object val)
+        {
+            return this.dataHelper.castBQNumeric(val);
+        }
+    }
+}
---
+++ frankfund/ServiceLayer/AnalyticsService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using DataAccessLayer;
+using Google.Cloud.BigQuery.V2;
+using DataAccessLayer.Models;
+using System.Collections.Generic;
+
+namespace ServiceLayer
+{
+    public class AnalyticsService
+    {
+        private readonly AnalyticsDataAccess dataAccess;
+
+        public AnalyticsService()
+        {
+            this.dataAccess = new AnalyticsDataAccess();
+        }
+
+        // Given an account ID, return a dictionary that maps spending category to the sum of
+        // expenses in that category for a particular user
+        public Dictionary<string,decimal> getSpendingPerCategory(long accID)
+        {
+            Dictionary<string, decimal> spendingPerCategory = new Dictionary<string, decimal>();
+            foreach (BigQueryRow row in dataAccess.getSpendingPerCategory(accID))
+            {
+                spendingPerCategory.Add(
+                    key: Convert.ToString(row["TransactionCategory"]),
+                    value: dataAccess.castBQNumeric(row["CategoryTotal"])
+                );
+            }
+
+            // No acount exists with the given accID
+            if(spendingPerCategory.Count == 0)
+            {
+                return null;
+            }
+            // Otherwise return the breakdown
+            else
+            {
+                return spendingPerCategory;
+            }
+        }
+
+        // Overloading wrapper
+        public Dictionary<string,decimal> getSpendingPerCategory(UserAccount acc)
+        {
+            return getSpendingPerCategory(acc.AccountID);
+        }
+    }
+}
