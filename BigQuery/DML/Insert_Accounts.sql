0ae37dc6df5c4efbe785caa25deea4dd349acc15
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-03-20 20:24:28 -0700

    Create Insert_Accounts.sql

---
+++ BigQuery/DML/Insert_Accounts.sql
@@ -0,0 +1,11 @@
+INSERT INTO FrankFund.Accounts
+VALUES (1, "AutumnNguyen", "a.nguyen@gmail.com", "password", Null, Null, Null);
+
+INSERT INTO FrankFund.Accounts
+VALUES (2, "DevinSuy", "d.suy@gmail.com", "password", Null, Null, Null);
+
+INSERT INTO FrankFund.Accounts
+VALUES (3, "RachelPai", "r.pai@gmail.com", "password", Null, Null, Null);
+
+INSERT INTO FrankFund.Accounts
+VALUES (4, "AutumnNguyen", "k.tran@gmail.com", "password", Null, Null, Null);
