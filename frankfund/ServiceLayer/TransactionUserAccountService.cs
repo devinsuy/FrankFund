369aa305ca24755bca62e378229977e1aefef9ed
kennethtran590 <48599140+kennethtran590@users.noreply.github.com>
2021-03-21 20:53:51 -0700

    FRAN-2 Implemented Transaction User Account service layer, implemented getTransactionsFromAccount()

---
+++ frankfund/ServiceLayer/TransactionUserAccountService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using DataAccessLayer;
+using Google.Cloud.BigQuery.V2;
+using DataAccessLayer.Models;
+using System.Collections.Generic;
+
+namespace ServiceLayer
+{
+    public class TransactionUserAccountService
+    {
+        private readonly TransactionUserAccountDataAccess dataAccess;
+
+        public TransactionUserAccountService()
+        {
+            this.dataAccess = new TransactionUserAccountDataAccess();
+        }
+
+        public List<Transaction> getTransactionsFromAccount(long accID)
+        {
+            List<Transaction> transactionsList = new List<Transaction>();
+
+            foreach (BigQueryRow row in this.dataAccess.getTransactionsFromAccount(accID))
+            {
+                Transaction transaction = null;
+                long SGID = -1;     // Nullable attr
+                if (row["SGID"] != null)
+                {
+                    SGID = (long)row["SGID"];
+                }
+                transaction = new Transaction(
+                    (long)row["TID"], (long)row["AccountID"], SGID,
+                    (string)row["TransactionName"],
+                    this.dataAccess.castBQNumeric(row["Amount"]),
+                    (DateTime)row["DateTransactionMade"],
+                    (DateTime)row["DateTransactionEntered"],
+                    (bool)row["IsExpense"],
+                    this.dataAccess.ParseEnum<transactionCategory>((string)row["TransactionCategory"])
+                );
+                transactionsList.Add(transaction);
+            }
+            return transactionsList;
+        }
+    }
+}
