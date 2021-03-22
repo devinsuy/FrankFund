525fed93b8ca7e16d9236d3679f16db9431a53c9
Autumn Nguyen <38737498+seasonfall27@users.noreply.github.com>
2021-03-22 13:14:10 -0700

    Update DDL for Accounts for Password Hash/Salt

--- BigQuery/DDL/Accounts_DDL.sql
+++ BigQuery/DDL/Accounts_DDL.sql
@@ -1,9 +1,10 @@
 ---ACCOUNTS DDL---
 CREATE TABLE FrankFund.Accounts (
-	AccountID	 	INT64	NOT NULL,
+	AccountID	 INT64	NOT NULL,
 	AccountUsername	STRING	NOT NULL,
 	EmailAddress	STRING	NOT NULL,
-	Password		STRING	NOT NULL,
+	PasswordHash	STRING	NOT NULL,
+	PasswordSalt	STRING	NOT NULL,
 	FacebookID		DECIMAL,		
 	GoogleID		DECIMAL		
 );
