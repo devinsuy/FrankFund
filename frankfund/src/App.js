a983001a4c95da646739ce4ef1083fd26e4093d9
Autumn Nguyen <38737498+seasonfall27@users.noreply.github.com>
2021-04-04 13:59:39 -0700

    Added CSS for createuseraccount and set up App.JS for landing page

--- frankfund/src/App.js
+++ frankfund/src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import CreateUserAccount from "./components/createuseraccount.component";
 
 function App() {
-  return (
-    <div className="App">
-       {/* Default code commented out for testing */}
-      {/*<header className="App-header">
+    return (<Router>
+    {/* Default code commented out for testing */ }
+    {/*<div className="App">
+      <header className="App-header">
        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -25,14 +27,33 @@ function App() {
       </header>*/}
 
       <div className="App">
-        {/* CreateUserAccount component 
+      {/* Landing Page and CreateUserAccount component 
           1. Set up HTML template code
           TODO:
           2. Add CSS
           3. Connect to backend */}
-        <CreateUserAccount />
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/create-user-account"}>FrankFund</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/create-user-account"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="outer">
+        <div className="inner">
+          <Switch>
+            <Route exact path='/' component={CreateUserAccount} />
+            <Route path="/create-user-account" component={CreateUserAccount} />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </div></Router>
   );
 }
 
