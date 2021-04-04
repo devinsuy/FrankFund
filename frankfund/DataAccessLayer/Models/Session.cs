99e566a6adaab301563e0891030a006dfc517099
Autumn Nguyen <38737498+seasonfall27@users.noreply.github.com>
2021-04-04 15:11:28 -0700

    Create Session.cs in Models

---
+++ frankfund/DataAccessLayer/Models/Session.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataAccessLayer.Models
+{
+    // Model for user session (when they are logged in using the application)
+    public class Session
+    {
+        // Session Attributes
+        public readonly long SessionID; // NEED TODO: getNextSessionID function()
+        public string JWTToken { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public DateTime DateIssued { get; set; }
+        [Required]
+        public DateTime DateExpired { get; set; }
+
+        public Session(string userName, string jwtToken)
+        {
+            this.UserName = userName;
+            this.JWTToken = jwtToken;
+            this.DateIssued = DateTime.UtcNow;
+            this.DateExpired = DateTime.UtcNow.AddMinutes(30);
+        }
+    }
+}
