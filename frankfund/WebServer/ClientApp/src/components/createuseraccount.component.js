046c5c00cfe5d1912047beaca233c60a0479a57d
Autumn Nguyen <38737498+seasonfall27@users.noreply.github.com>
2021-04-12 12:41:23 -0700

    Rewrote onSubmit function for createuseraccount comp

--- frankfund/WebServer/ClientApp/src/components/createuseraccount.component.js
+++ frankfund/WebServer/ClientApp/src/components/createuseraccount.component.js
@@ -31,17 +31,22 @@ export default class CreateUserAccount extends Component {
     //    this.setState({ description: '' });
     //}
 
-    onSubmit(e) {
-        e.preventDefault();
+    onSubmit(e, form) {
+        //e.preventDefault();
         // Simple POST request with a JSON body using fetch
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ title: 'React POST Request Example' })
-        };
-        fetch('api/account/create', requestOptions)
-            .then(response => response.json())
-            .then(data => this.setState({ AccountUsername: data.username, EmailAddress: data.email, PasswordHash: data.password }));
+        //const requestOptions = {
+        //    method: 'POST',
+        //    headers: { 'Content-Type': 'application/json' },
+        //    body: JSON.stringify({ title: 'React POST Request Example' })
+        //};
+        //fetch('api/account/create&apikey={apiKey}', requestOptions)
+        //    .then(response => response.json())
+        //    .then(data => this.setState({ AccountUsername: data.username, EmailAddress: data.email, PasswordHash: data.password }));
+
+        fetch(form.action, { method: 'post', body: new FormData(form) });
+
+        console.log('We send post asynchronously (AJAX)');
+        e.preventDefault();
     }
 
 
@@ -49,9 +54,7 @@ export default class CreateUserAccount extends Component {
         return (
             <form
                 id="create-user-account"
-                action='api/account/create'
-                method= 'post'
-                onSubmit={this.onSubmit}>
+                method="POST" action="api/account/create&apikey=bd0eecf7cf275751a421a6101272f559b0391fa0" onsubmit="send(event,this)">
                 <h3>Create New User Account</h3>
 
                 <div className="form-group">
@@ -66,7 +69,7 @@ export default class CreateUserAccount extends Component {
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="PasswordHash" className="form-control" placeholder="Enter password" />
+                    <input type="Password" className="form-control" placeholder="Enter password" />
                 </div>
 
                 <button type="submit" className="btn btn-dark btn-lg btn-block">Register</button>
