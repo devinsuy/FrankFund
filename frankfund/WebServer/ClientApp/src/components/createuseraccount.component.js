b3ea41ae11a5370cc23f933dcc2c59e90a1eced0
Autumn Nguyen <38737498+seasonfall27@users.noreply.github.com>
2021-04-12 16:10:19 -0700

    Clean up commented code

--- frankfund/WebServer/ClientApp/src/components/createuseraccount.component.js
+++ frankfund/WebServer/ClientApp/src/components/createuseraccount.component.js
@@ -18,53 +18,6 @@ export default class CreateUserAccount extends Component {
     //    Password: ''
     //}
 
-    //handleChange = (e) => {
-    //    const name = e.target.name;
-    //    const value = e.target.value;
-    //    this.setState({
-    //        [name]: value
-    //    });
-    //}
-
-    //onSubmit = event => {
-    //    event.preventDefault();
-
-    //    user = {
-    //        AccountUsername: this.state.AccountUsername,
-    //        EmailAddress: this.state.EmailAddress,
-    //        Password: this.state.Password
-    //    };
-
-    //    //axios.post(`api/account/create&apikey=bd0eecf7cf275751a421a6101272f559b0391fa0`, { user })
-    //    //    .then(res => {
-    //    //        console.log(res);
-    //    //        console.log(res.data);
-    //    //    })
-
-    //    axios({
-    //        method: 'post',
-    //        url: '/api/account/create&apikey=bd0eecf7cf275751a421a6101272f559b0391fa0',
-    //        body: user,
-    //        headers: {
-    //            'Accept': 'application/json',
-    //            'Content-Type': 'application/json'
-    //        }
-    //    })
-    //        .then(res => {
-    //            if (res.status === 200) {
-    //                console.log(res)
-    //            }
-    //        }).catch(err => {
-    //            console.error(err);
-    //        });
-    //}
-
-    //onChange(e) {
-    //    this.setState({
-    //        [e.target.name]: e.target.value
-    //    });
-    //}
-
     // ----- Testing with static JSON input -----
     //onSubmit(e) {
     //    e.preventDefault();
@@ -106,12 +59,6 @@ export default class CreateUserAccount extends Component {
             .catch((err) => {
                 throw err;
             });
-        //axios.post('/api/account/create&apikey=bd0eecf7cf275751a421a6101272f559b0391fa0', json)
-        //    .then(response => {
-        //        console.log(response)
-        //    }).catch(error => {
-        //        console.log(error.response)
-        //    })
     }
 
     render() {
