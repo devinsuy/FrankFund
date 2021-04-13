802fed4acccc3a5e125ee2a7e51f0bfe16910166
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-04-12 18:16:48 -0700

    Added PATCH by username

--- frankfund/WebServer/Controllers/UserAccountController.cs
+++ frankfund/WebServer/Controllers/UserAccountController.cs
@@ -188,7 +188,7 @@ namespace REST.Controllers
                 return BadRequest();
             }
 
-            // Validate the attributes of the PATCH request, each attribute specified
+            // Validate the attributes of the PATCH request, each attribute specified, given account id
             // in the request must be an attribute of a Account
             Dictionary<string, object> req = JsonConvert.DeserializeObject<Dictionary<string, object>>(Convert.ToString(reqBody));
             HashSet<string> reqAttributes = new HashSet<string>(req.Keys);
@@ -202,7 +202,66 @@ namespace REST.Controllers
             // Http POST cannot update a Account that does not exist
             if (acc == null)
             {
-                return NotFound();
+                return NotFound($"No account exists with AccountID {accID}");
+            }
+
+            // Otherwise fufill the POST request and update the corresponding Account
+            // Http POST may only specify a few attributes to update or provide all of them
+
+            // Update the Account with the specified POST attributes
+            try
+            {
+                if (reqAttributes.Contains("AccountUsername"))
+                {
+                    acc.AccountUsername = Convert.ToString(req["AccountUsername"]);
+                }
+                if (reqAttributes.Contains("EmailAddress"))
+                {
+                    acc.EmailAddress = Convert.ToString(req["EmailAddress"]);
+                }
+                if (reqAttributes.Contains("Password"))
+                {
+                    acc.PasswordHash = Convert.ToString(req["Password"]);
+                }
+            }
+            // Formatting or improper data typing raised exception, bad request
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return BadRequest();
+            }
+
+            // Write changes, if any
+            uas.update(acc);
+            return new OkResult();
+        }
+
+        // Modify an existing Account without specifying all attributes in payload, given account username
+        // returns Http 404 Not found if doesn't exist
+        [Route("api/account/user={user}&apikey={apiKey}")]
+        [HttpPatch]
+        public IActionResult UpdateByUsername(string user, string apiKey, [FromBody] JsonElement reqBody)
+        {
+            if (!api.validAPIKey(apiKey))
+            {
+                return new UnauthorizedObjectResult("Invalid API key");
+            }
+
+            // Validate the attributes of the PATCH request, each attribute specified, given account id
+            // in the request must be an attribute of a Account
+            Dictionary<string, object> req = JsonConvert.DeserializeObject<Dictionary<string, object>>(Convert.ToString(reqBody));
+            HashSet<string> reqAttributes = new HashSet<string>(req.Keys);
+            if (!api.validAttributes(attributes, reqAttributes))
+            {
+                return BadRequest();
+            }
+
+            UserAccount acc = uas.getUsingUsername(user);
+
+            // Http POST cannot update a Account that does not exist
+            if (acc == null)
+            {
+                return NotFound($"No account exists with useruname {user}");
             }
 
             // Otherwise fufill the POST request and update the corresponding Account
