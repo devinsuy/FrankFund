27e59887e3b0151151b2aeda7c0dad857b5a3b21
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-04-23 16:14:03 -0700

    Move endpoints for transactions and savingsgoals for a particular account into separate controllers

---
+++ frankfund/WebServer/Controllers/UserAccountSavingsGoalsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ServiceLayer;
+
+namespace REST.Controllers
+{
+    public class UserAccountSavingsGoalsController : ControllerBase
+    {
+        private readonly ILogger<UserAccountSavingsGoalsController> _logger;
+        private readonly APIHelper api;
+        private readonly UserAccountService uas;
+
+        public UserAccountSavingsGoalsController(ILogger<UserAccountSavingsGoalsController> logger)
+        {
+            _logger = logger;
+            api = new APIHelper();
+            uas = new UserAccountService();
+        }
+
+
+        // ------------------------------ Account SavingsGoals Endpoints ------------------------------
+
+        // Serve all SavingsGoals associated with a given Account ID
+        [Route("api/account/accID={accID}/SavingsGoals&apikey={apiKey}")]
+        [HttpGet]
+        public IActionResult getGoals(long accID, string apiKey)
+        {
+            if (!api.validAPIKey(apiKey))
+            {
+                return new UnauthorizedObjectResult("Invalid API key");
+            }
+            if (accID < 1)
+            {
+                return BadRequest();
+            }
+            return api.serveJson(uas.getSavingsGoalsFromAccount(accID));
+        }
+
+        [Route("api/account/user={user}/SavingsGoals&apikey={apiKey}")]
+        [HttpGet]
+        public IActionResult getGoals(string user, string apiKey)
+        {
+            if (!api.validAPIKey(apiKey))
+            {
+                return new UnauthorizedObjectResult("Invalid API key");
+            }
+            return api.serveJson(uas.getSavingsGoalsFromAccount(user));
+        }
+
+
+    }
+}
---
+++ frankfund/WebServer/Controllers/UserAccountTransactionsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ServiceLayer;
+
+namespace REST.Controllers
+{
+    public class UserAccountTransactionsController : ControllerBase
+    {
+        private readonly ILogger<UserAccountTransactionsController> _logger;
+        private readonly APIHelper api;
+        private readonly UserAccountService uas;
+
+        public UserAccountTransactionsController(ILogger<UserAccountTransactionsController> logger)
+        {
+            _logger = logger;
+            api = new APIHelper();
+            uas = new UserAccountService();
+        }
+
+
+        // ------------------------------ Account Transaction Endpoints ------------------------------
+
+        // Serve all Transactions associated with a given AccountID
+        [Route("api/account/accID={accID}/Transactions&apikey={apiKey}")]
+        [HttpGet]
+        public IActionResult getTransactions(long accID, string apiKey)
+        {
+            if (!api.validAPIKey(apiKey))
+            {
+                return new UnauthorizedObjectResult("Invalid API key");
+            }
+            return api.serveJson(uas.getTransactionsFromAccount(accID));
+        }
+
+        // Serve all Transactions associated with a given username
+        [Route("api/account/user={user}/Transactions&apikey={apiKey}")]
+        [HttpGet]
+        public IActionResult getTransactions(string user, string apiKey)
+        {
+            if (!api.validAPIKey(apiKey))
+            {
+                return new UnauthorizedObjectResult("Invalid API key");
+            }
+            return api.serveJson(uas.getTransactionsFromAccount(user));
+        }
+
+    }
+}
