c2813a9eed9f31d91d55da1f5f777d514086c4ad
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-04-26 01:50:00 -0700

    Setup boilerplate

--- frankfund/WebServer/ClientApp/src/components/trends.component.js
+++ frankfund/WebServer/ClientApp/src/components/trends.component.js
@@ -1,5 +1,46 @@
-export default function Trends(){
-    return <>
-        <button type="submit" className="btn btn-dark btn-lg btn-block">TODO</button>
-    </>
+import React, { Component, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    type: PropTypes.bool,
+};
+  
+const defaultProps = {
+    type: undefined,
+};
+
+export default class Trends extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      foo: "bar"
+    };
+  }
+  
+  // const [stateItem, setStateItem] = useState(initialState);
+
+  componentDidMount() {
+  }
+  
+  // useEffect(() => {
+  //   update.item = `{stateItem} to update`;
+  // });
+
+
+  render() {
+    const { desctructureThis } = this; 
+    const { destructureProps } = this.props;
+    const { destructureState } = this.state;
+
+    return (
+        <div className="container">
+            <h1 class="display-4 font-weight-bold white-text pt-5 mb-2">Trends</h1>
+            <p>TODO</p>
+        </div>
+    );
+  }
 }
+
+Trends.propTypes = propTypes;
+Trends.defaultProps = defaultProps;
